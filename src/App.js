581118ba29fbb2d6efa08df4a33d05b951039815
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-11-07 00:58:43 +0530

    blog page done

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
-import { BrowserRouter as Router, Routes, Link , Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
 function App() {
   return (
-      <>
-       
+    <>
       <div className="App">
-      <Router>
-        <Routes>
-        <Route path="/" element={<Home/>}/>
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/experiences" element={<Resume/>} />
-          <Route path="/blog" element={<Blog/>} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experiences" element={<Resume />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
         </Router>
       </div>
-      
-      </>
-   
+    </>
   );
 }
 
