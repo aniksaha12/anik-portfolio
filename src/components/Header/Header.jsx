3bd433ee1f092bbeec875a363ac1a7b1cd28520a
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-11-08 00:38:05 +0530

    bootstrap link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,6 +21,7 @@ export default function Header() {
           href="https://fonts.gstatic.com/"
           crossOrigin=""
         />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400;500;600;700;800;900&family=Urbanist:wght@400;500;600;700&display=swap"
           rel="stylesheet"
